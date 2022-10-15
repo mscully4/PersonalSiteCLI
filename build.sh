60779ea24dae8e840bdad187e2009cc68da8d3d0
Michael <michael.scully1997@gmail.com>
2022-10-14 23:44:11 -0400

    Comment out ISORT for Now
    Experiencing conflcts between ISORT and black so commenting it out for
    now

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-set e
+set -e
 
 echo "Running Black"
 black ./personal-site-cli --check
 printf "\n"
 
-echo "Running isort"
-(cd ./personal-site-cli && python3 -m isort --check-only --recursive .)
-printf "\n"
+# echo "Running isort"
+# (cd ./personal-site-cli && python3 -m isort --check-only --recursive .)
+# printf "\n"
 
 echo "Running Flake8"
 flake8 ./personal-site-cli
